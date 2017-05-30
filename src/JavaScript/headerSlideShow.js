5b6ad57e14a7268e4da58a7e3325a930c826e5b7
Kole Barnes <kbarnes261@gmail.com>
2017-05-30 13:08:31 -0700

    Downloaded animate.css and bootStrapNavBarAnimate.js to have locally. Created footerSocial.html, footerSponsors.html and header.html to be "included" in html. Had to take headerSlidShow out of the "included" HTML because of bugs. Got ride of .activeDates and did it the right way with .activeSchedule>div>ul. Cleaned up messy code in headerSlidShow.js. Changed some class and id names to better match what they are. Cleaned up SISLRATEST.css to take up less line space.

--- src/JavaScript/headerSlideShow.js
+++ src/JavaScript/headerSlideShow.js
@@ -1,10 +1,7 @@
-$(".slideshow > img:gt(0)").hide();
+startSlideShow();
 
-setInterval(function() {
-  $('.slideshow > img:first')
-  .fadeOut(1500)
-  .next()
-  .fadeIn(1500)
-  .end()
-  .appendTo('.slideshow');
-  }, 8000);
+function startSlideShow() {
+  $(".slideshow > img:gt(0)").hide();
+  setInterval(headerSlideShow, 6000);
+  function headerSlideShow() {$('.slideshow > img:first').fadeOut(1500).next().fadeIn(1500).end().appendTo('.slideshow');}
+}
