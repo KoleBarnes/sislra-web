92ee63b4087b5780d4d623513be99ebd09ee2aac
KoleBarnes <kbarnes261@gmail.com>
2019-03-26 09:12:44 -0700

    Updated race dates, posters and index main content.

--- src/JavaScript/expire.js
+++ src/JavaScript/expire.js
@@ -3,7 +3,7 @@ function expire() {
   var currentTime = new Date();
   var expiry  = new Date('10/23/2018');
   var SilentAuctionEnd = new Date('12/02/2018');
-  var expiry3 = new Date('04/29/2018');
+  var expiry2 = new Date('04/07/2019');
   
   // var expiry2 = new Date('04/22/2017 12:00 AM')
 
@@ -11,13 +11,13 @@ function expire() {
     $('#success').show();
   } */
   
-/*   if((expiry.getTime() < currentTime.getTime() ) && (currentTime.getTime() < expiry2.getTime() )) {
+  if((expiry.getTime() < currentTime.getTime() ) && (currentTime.getTime() < expiry2.getTime() )) {
     $('#content').show();
-  } */
+  } 
 
-  if (currentTime.getTime() < SilentAuctionEnd.getTime() ) {
+  /* if (currentTime.getTime() < SilentAuctionEnd.getTime() ) {
     $('#SilentAuctionEnd').show();
-  }
+  } */
   
 };
 
